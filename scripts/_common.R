bf55ea1e67afdc3a30fb6d14e088b9fbb737c761
zief0002 <zief0002@umn.edu>
2023-07-25 16:51:13 -0500

    Updated book

---
+++ scripts/_common.R
@@ -0,0 +1,40 @@
+# Load libraries
+# library(gt)
+# library(gtExtras)
+library(knitr)
+library(kableExtra)
+library(patchwork)
+
+
+# Set knitr print options
+opts_knit$set(
+  width = 85, 
+  tibble.print_max = Inf
+)
+
+
+# Set knitr chunk options
+opts_chunk$set(
+  
+  # General
+  comment = NA,
+  message = FALSE, 
+  warning = FALSE,
+  tidy = FALSE,
+  
+  # Figures
+  fig.align = 'center',
+  fig.height = 6,
+  fig.width = 6,  
+  out.width = '50%' 
+)
+
+
+# Set global options
+options(
+  htmltools.dir.version = FALSE,
+  tibble.pillar.subtle = FALSE, 
+  tibble.pillar.sigfig = 7, 
+  tibble.pillar.min_title_chars = 10,
+  scipen = 5
+)
